90bb14c5657d73d5a389da7899e75f083c5224db
wqdygkd <cui3155@gmail.com>
2024-01-14 12:39:29 +0800

    更新

---
+++ debug.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         脚本调试
+// @namespace    https://your.site/
+// @version      0.1.0
+// @description  What does your script do
+// @author       You
+// @include *
+// @match        ://*/*
+// @grant        GM_addElement
+// @noframes
+// @run-at       document-start
+// @grant        unsafeWindow
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @require      https://cdn.jsdelivr.net/npm/vue@3.2.6/dist/vue.global.min.js
+// @require      file://D:\project\tool\packages\tampermonkey-script\dist\yourscript.user.js
+// ==/UserScript==
+
+(function () {
+  'use strict'
+  // Your code here...
+})()
---
+++ src/components/console.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-config-provider size="small">
+    <el-checkbox v-model="status" label="启用" />
+  </el-config-provider>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+
+const storageKey = 'winex.console'
+let status = ref<boolean>(GM_getValue(storageKey, false))
+watch(status, (newStatus) => {
+  console.log('watch')
+  GM_setValue(storageKey, newStatus)
+})
+</script>
