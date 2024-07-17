ce561fd61c687b49f63368d94cd6c61b4bb037b2
cuilongjin <cui3155@gmail.com>
2024-07-17 13:53:29 +0800

    更新

--- src/meta/common.meta.ts
+++ src/meta/common.meta.ts
@@ -1,12 +1,12 @@
 export default {
   name: 'Winex助手',
   namespace: 'Winex',
-  version: '0.0.0',
+  version: '0.0.1',
   description: 'Winex助手',
   author: 'zhang333',
   match: ['://*/*'],
   'run-at': 'document-start',
   grant: ['unsafeWindow', 'GM_getValue', 'GM_setValue', 'GM_registerMenuCommand'],
-  require: ['https://cdn.jsdelivr.net/npm/vue@3.4.20/dist/vue.global.js'],
+  require: ['https://cdn.jsdelivr.net/npm/vue@3.4.31/dist/vue.global.js'],
   icon: 'https://weberp.winning.com.cn/favicon.ico'
 }
