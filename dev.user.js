a4b165eedd555e771e606a02d6475cd3471f1089
wqdygkd <cui3155@gmail.com>
2026-01-25 17:45:15 +0800

    chore(dev): 更新开发模式配置
    更新脚本元数据信息，包括名称、版本和描述
    移除本地IP匹配规则，统一使用通配符匹配所有域名
    移除Vue依赖引入，简化开发环境配置
    修复脚本元素添加时的语法格式

--- dev.user.js
+++ dev.user.js
@@ -1,20 +1,15 @@
 // ==UserScript==
-// @name         Your Script (dev mode)
-// @namespace    https://your.site/
-// @version      0.1.0
-// @description  What does your script do
+// @name         Winex助手(dev)
+// @namespace    Winex
+// @version      0.0.0
+// @description  Winex助手
 // @author       zhang333
-// // @match        ://*/*
-// @match        http://localhost:*/*
-// @match        http://127.0.0.1:*/*
-// @match        http://172.16.7.60:*/*
+// @match        ://*/*
 // @grant        GM_addElement
-// @icon
 // @run-at       document-start
 // @grant        unsafeWindow
 // @grant        GM_setValue
 // @grant        GM_getValue
-// @require      https://cdn.jsdelivr.net/npm/vue@3.4.13/dist/vue.global.js
 // ==/UserScript==
 
 (function () {
@@ -22,12 +17,12 @@
   // source: https://cn.vitejs.dev/guide/backend-integration.html
   GM_addElement('script', {
     src: 'http://localhost:5173/@vite/client',
-    type: 'module'
+    type: 'module',
   })
 
   GM_addElement('script', {
     src: 'http://localhost:5173/src/main.ts',
-    type: 'module'
+    type: 'module',
   })
 
   // GM_registerMenuCommand('设置', function () {
