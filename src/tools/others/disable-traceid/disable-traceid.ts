84245036a17e24a8f3f87f020106c798d09f34ff
wqdygkd <cui3155@gmail.com>
2026-04-27 19:02:21 +0800

    更新

--- src/tools/others/disable-traceid/disable-traceid.ts
+++ src/tools/others/disable-traceid/disable-traceid.ts
@@ -7,10 +7,12 @@ export function init() {
 
   if (!storage.enable) return
 
-  const hookedKey = Symbol.for('url-search-params-set-hooked')
+  const searchParamsSetHookedKey = Symbol.for('url-search-params-set-hooked')
+  const urlToStringHookedKey = Symbol.for('url-to-string-hooked')
   const proto = unsafeWindow.URLSearchParams.prototype as URLSearchParams & Record<symbol, boolean>
+  const urlProto = unsafeWindow.URL.prototype as URL & Record<symbol, boolean>
 
-  if (!proto[hookedKey]) {
+  if (!proto[searchParamsSetHookedKey]) {
     const originalSet = proto.set
     proto.set = function (key, value) {
       if (String(key).toLowerCase() === 'traceid') {
@@ -18,7 +20,23 @@ export function init() {
       }
       return originalSet.call(this, key, value)
     }
-    Object.defineProperty(proto, hookedKey, {
+    Object.defineProperty(proto, searchParamsSetHookedKey, {
+      value: true,
+    })
+  }
+
+  if (!urlProto[urlToStringHookedKey]) {
+    const originalToString = urlProto.toString
+    urlProto.toString = function () {
+      const url = new unsafeWindow.URL(originalToString.call(this))
+      Array.from(url.searchParams.keys()).forEach((key) => {
+        if (key.toLowerCase() === 'traceid') {
+          url.searchParams.delete(key)
+        }
+      })
+      return originalToString.call(url)
+    }
+    Object.defineProperty(urlProto, urlToStringHookedKey, {
       value: true,
     })
   }
