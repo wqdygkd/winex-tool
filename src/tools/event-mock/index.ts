5b627c8ba73232eede7f1257662cd1f22ae89784
wqdygkd <cui3155@gmail.com>
2026-05-31 20:29:37 +0800

    feat(event-mock): add preset data import on init

--- src/tools/event-mock/index.ts
+++ src/tools/event-mock/index.ts
@@ -1,6 +1,8 @@
 import type { ToolModule } from '../registry'
 import { context } from './core/context'
 import { ConfigStorage } from './storage/config'
+import { TemplateStorage } from './storage/template'
+import { importPresetData } from './presets'
 import EventMockUI from './ui/event-mock.vue'
 
 const name = '事件模拟(New)'
@@ -8,7 +10,12 @@ const storageKey = `${__namespace}event-mock1`
 
 function init() {
   const configStorage = new ConfigStorage()
+  const templateStorage = new TemplateStorage()
+
   configStorage.load()
+
+  // 自动导入预制数据
+  importPresetData(templateStorage)
 }
 
 EventMockUI.name = name
